80ba9e62f6004bfee25a6293f83a8079ae7eb5f0
Marcin <kuklam@op.pl>
2021-02-01 09:37:25 +0100

    image deleted

--- src/tests/languageExamplesForTest.ts
+++ src/tests/languageExamplesForTest.ts
@@ -4,13 +4,13 @@ const PL: Language = {
     active: true,
     languageCode: 'PL',
     languageName: 'polski',
-    flagUrl: ''
+    flagUrl: '\\images\\1612045171756polska_flaga.jpg'
 }
 const CZE: Language = {
     active: true,
     languageCode: 'CZE',
     languageName: 'polski',
-    flagUrl: ''
+    flagUrl: '\\images\\1612045202729czeska_flaga.png'
 }
 const Languages: Language[] = [PL, CZE];
 export {Languages};
